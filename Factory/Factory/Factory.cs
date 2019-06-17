981cb39e448551f25cf18f01d8a68e1a10061954
tolga tunçel <48061748+TolgaTuncel@users.noreply.github.com>
2019-06-18 02:31:46 +0300

    Factory

---
+++ Factory/Factory/Factory.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Factory
+{
+    class BildirimManagerFactory
+    {
+        private IBildirimFactory _bildirimFactory;
+
+        public BildirimManagerFactory(IBildirimFactory bildirimFactory)
+        {
+            _bildirimFactory = bildirimFactory;
+        }
+
+        public void Save()
+        {
+            IBildirim bildirim = _bildirimFactory.BildirimOlustur();
+            bildirim.Log();
+        }
+    }
+
+    public class LoggerFactory1 : IBildirimFactory
+    {
+        public IBildirim BildirimOlustur()
+        {
+           
+            return new MailBildirim();
+        }
+    }
+
+    public class LoggerFactory2 : IBildirimFactory
+    {
+        public IBildirim BildirimOlustur()
+        {
+           
+            return new SmsBildirim();
+        }
+    }
+
+    public interface IBildirimFactory
+    {
+        IBildirim BildirimOlustur();
+    }
+
+    public interface IBildirim
+    {
+        void Log();
+    }
+
+    public class MailBildirim : IBildirim
+    {
+        public void Log()
+        {
+            Console.WriteLine("Mail Gönderme İşlemi Başarılı");
+        }
+    }
+
+    public class SmsBildirim : IBildirim
+    {
+        public void Log()
+        {
+            Console.WriteLine("SMS Gönderme İşlemi Başarılı");
+        }
+    }
+}
