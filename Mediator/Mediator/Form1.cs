592ad7b5729f928d082cd0442003a5535b1b8efb
tolga tunçel <48061748+TolgaTuncel@users.noreply.github.com>
2019-06-18 02:32:43 +0300

    Mediator

---
+++ Mediator/Mediator/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mediator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Mediatorr mediator = new Mediatorr();
+            Hoca tolga = new Hoca(mediator);
+
+            tolga.Name = "Tolga";
+
+            mediator.Hoca = tolga;
+            Ogrenci refik = new Ogrenci();
+            refik.Name = "Refik";
+
+
+            Ogrenci salih = new Ogrenci();
+            salih.Name = "Salih";
+            mediator.Ogrenciler = new List<Ogrenci> { refik, salih };
+
+            tolga.SendNewImageUrl("slide1.jpg");
+            MessageBox.Show(tolga.Name + "Hoca resmi değiştirdi");
+            MessageBox.Show("Resim" + salih.Name + "adlı öğrenciye iletildi");
+            MessageBox.Show("Resim" + refik.Name + "adlı öğrenciye iletildi");
+            //birşeyler yazdı.
+        }
+    }
+}
---
+++ Mediator/Mediator/MediatorPattern.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator
+{
+    class MediatorPattern
+    {
+        
+    }
+   abstract class KursUyesi
+         {
+
+        protected Mediatorr Mediator;
+
+        protected KursUyesi (Mediatorr mediator)
+        {
+            Mediator = mediator;
+        }
+    }
+
+    class Hoca : KursUyesi
+    {
+        private new Mediatorr Mediator;
+
+        public string Name { get; internal set; }
+
+        public void SendNewImageUrl(string url)
+        {
+            
+            Mediator.UpdateImage(url);
+
+        }
+
+        public void AnswerQuestion(string answer, Ogrenci ogrenci)
+        {
+            
+        }
+        public Hoca(Mediatorr mediator) : base(mediator)
+        {
+          
+
+        }
+        internal void RecieveQuestion(string question, Ogrenci ogrenci)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void RecieveQuestion(string question, object ogrenci)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Ogrenci : KursUyesi
+    {
+        private static Mediatorr mediator;
+
+        public Ogrenci(Mediatorr mediator) : base(mediator)
+        {
+
+        }
+
+        public Ogrenci() : base(Mediator1)
+        {
+        }
+
+        public void RecieveImage(string url)
+        {
+           
+        }
+
+        public void RecieveAnswer()
+        {
+           
+        }
+
+        public string isim { get; set; }
+        public string Name { get; internal set; }
+        internal static Mediatorr Mediator1 { get => mediator; set => mediator = value; }
+    }
+
+    class Mediatorr
+    {
+        public Hoca Hoca { get; set;}
+        public List<Ogrenci> Ogrenciler { get; set; }
+
+        public void UpdateImage(string url)
+        {
+            foreach (var ogrenci in Ogrenciler)
+            {
+                ogrenci.RecieveImage(url);
+
+            }
+
+        }
+
+        public void SendQuestion(string question, Ogrenci ogrenci)
+        {
+
+            Hoca.RecieveQuestion(question, ogrenci);
+        }
+
+        public void SendAnswer(string answer, Ogrenci ogrenci)
+        {
+
+            ogrenci.RecieveAnswer();
+        }
+    }
+
+
+}
