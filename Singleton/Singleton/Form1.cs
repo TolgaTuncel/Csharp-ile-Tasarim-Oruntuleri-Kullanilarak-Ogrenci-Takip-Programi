c66803307b9b2ef71f1b5b33d292c513d6e440f1
tolga tunçel <48061748+TolgaTuncel@users.noreply.github.com>
2019-06-18 02:35:01 +0300

    Singleton

---
+++ Singleton/Singleton/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Singleton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //SİNGLETON PATTERN KULLANIMI
+            var ogrenciManager = OgrenciManagerSingleton.CreateSingleton();
+            ogrenciManager.Save();
+
+            MessageBox.Show("Öğrenci Bilgileri Kaydedildi");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Singleton/Singleton/OgrenciManagerSingleton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class OgrenciManagerSingleton
+    {
+        private static OgrenciManagerSingleton _ogrManager;
+    
+        static object _lockObject = new object();
+
+        private OgrenciManagerSingleton() { }
+
+        public static OgrenciManagerSingleton CreateSingleton()
+        {
+       
+
+            lock (_lockObject)
+            {
+                if (_ogrManager == null)
+                {
+                    _ogrManager = new OgrenciManagerSingleton();
+                }
+            }
+
+            return _ogrManager;
+        }
+
+       
+        public void Save()
+        {
+            
+        }
+    }
+}
