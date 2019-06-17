5f2fa92698a0733847470121dbe688ab6da3740a
tolga tunçel <48061748+TolgaTuncel@users.noreply.github.com>
2019-06-18 02:32:22 +0300

    Iterator

---
+++ Iterator/Iterator/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Iterator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PersonelAggregate aggregate = new PersonelAggregate();
+            aggregate.Add(new Iterator { Id = 1, DersAdi = "Dil ve Anlatım", DersSaati = "2" });
+            aggregate.Add(new Iterator { Id = 2, DersAdi = "Türk Edebiyatı", DersSaati = "2" });
+            aggregate.Add(new Iterator { Id = 3, DersAdi = "Yabancı Dil", DersSaati = "4" });
+            aggregate.Add(new Iterator { Id = 4, DersAdi = "Matematik", DersSaati = "6" });
+            aggregate.Add(new Iterator { Id = 5, DersAdi = "Fizik", DersSaati = "5" });
+            aggregate.Add(new Iterator { Id = 6, DersAdi = "Kimya", DersSaati = "4" });
+            aggregate.Add(new Iterator { Id = 7, DersAdi = "Biyoloji", DersSaati = "4" });
+            aggregate.Add(new Iterator { Id = 8, DersAdi = "Meslek Gelişim", DersSaati = "8" });
+
+            Iteratorr iterasyon = aggregate.CreateIterator();
+            while (iterasyon.HasItem())
+            {
+                MessageBox.Show($"ID : {iterasyon.CurrentItem().Id}\nDers Adı : {iterasyon.CurrentItem().DersAdi}\nDers Saati : {iterasyon.CurrentItem().DersSaati}\n*****");
+                iterasyon.NextItem();
+            }
+        }
+    }
+}
---
+++ Iterator/Iterator/Iterator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    struct Iterator
+    {
+        public int Id { get; set; }
+        public string DersAdi { get; set; }
+        public string DersSaati { get; set; }
+    }
+
+    interface IAggregate
+    {
+        Iteratorr CreateIterator();
+    }
+
+    interface Iteratorr
+    {
+        //Derse bak
+        bool HasItem();
+        //Dersi al
+        Iterator NextItem();
+        //Dersi getir.
+        Iterator CurrentItem();
+    }
+
+    class PersonelAggregate : IAggregate
+    {
+        List<Iterator> PersonelListesi = new List<Iterator>();
+        public void Add(Iterator Model) => PersonelListesi.Add(Model);
+        public Iterator GetItem(int index) => PersonelListesi[index];
+        public int Count { get => PersonelListesi.Count; }
+        public Iteratorr CreateIterator() => new PersonelIterator(this);
+    }
+
+    class PersonelIterator : Iteratorr
+    {
+        PersonelAggregate aggregate;
+        int currentindex;
+        public PersonelIterator(PersonelAggregate aggregate) => this.aggregate = aggregate;
+        public Iterator CurrentItem() => aggregate.GetItem(currentindex);
+        public bool HasItem()
+        {
+            if (currentindex < aggregate.Count)
+                return true;
+            return false;
+        }
+        public Iterator NextItem()
+        {
+            if (HasItem())
+                return aggregate.GetItem(currentindex++);
+            return new Iterator();
+        }
+    }
+}
