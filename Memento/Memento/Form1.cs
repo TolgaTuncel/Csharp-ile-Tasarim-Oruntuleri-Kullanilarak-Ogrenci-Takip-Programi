3dc2bd755963cbd84ebceb44603c6e8fc41aba61
tolga tunçel <48061748+TolgaTuncel@users.noreply.github.com>
2019-06-18 02:33:09 +0300

    Memento

---
+++ Memento/Memento/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memento
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GoGo derssinif = new GoGo();
+
+            derssinif.Sinif = "304";
+
+            GoGoCareTaker Taker = new GoGoCareTaker();
+
+            Taker.Memento = derssinif.Kaydet();
+
+
+
+
+            derssinif.OncekiniYukle(Taker.Memento);
+
+
+            MessageBox.Show(derssinif.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GoGo derssinif = new GoGo();
+
+            derssinif.Sinif = "305";
+
+            GoGoCareTaker Taker = new GoGoCareTaker();
+
+            Taker.Memento = derssinif.Kaydet();
+
+            derssinif.OncekiniYukle(Taker.Memento);
+
+            MessageBox.Show(derssinif.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            GoGo derssinif = new GoGo();
+
+            derssinif.Sinif = "306";
+
+            GoGoCareTaker Taker = new GoGoCareTaker();
+
+            Taker.Memento = derssinif.Kaydet();
+
+            derssinif.OncekiniYukle(Taker.Memento);
+
+            MessageBox.Show(derssinif.ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            GoGo derssinif = new GoGo();
+
+            derssinif.Sinif = "307";
+
+            GoGoCareTaker Taker = new GoGoCareTaker();
+
+            Taker.Memento = derssinif.Kaydet();
+
+            derssinif.OncekiniYukle(Taker.Memento);
+
+            MessageBox.Show(derssinif.ToString());
+        }
+    }
+}
---
+++ Memento/Memento/Memento.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    class GoGoMemento
+    {
+        public string Sinif { get; set; }
+    }
+
+    class GoGoCareTaker
+    {
+        public GoGoMemento Memento { get; set; }
+    }
+
+    class GoGo
+    {
+
+        public string Sinif { get; set; }
+
+        public override string ToString()
+        {
+            return $" Hoca {Sinif} 'te.";
+        }
+
+
+        public GoGoMemento Kaydet()
+        {
+            return new GoGoMemento
+            {
+                Sinif = this.Sinif,
+
+            };
+        }
+
+        public void OncekiniYukle(GoGoMemento Memento)
+        {
+            this.Sinif = Memento.Sinif;
+
+        }
+    }
+}
